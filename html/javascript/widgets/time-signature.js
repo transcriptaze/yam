d80279f940367c25d03030ca482a13873a5b3032
trancriptaze <transcriptaze.development@gmail.com>
2025-10-24 10:52:53 -0700

    time-signature: removed last remaining bits of figura <input> (cf. https://github.com/transcriptaze/yam/issues/13)

--- html/javascript/widgets/time-signature.js
+++ html/javascript/widgets/time-signature.js
@@ -85,14 +85,6 @@ export class TimeSignature extends HTMLElement {
     },
 
     figura: {
-      // input: () => {
-      //   const figura = this.shadowRoot.querySelector('input#figura')
-      //   if (figura.checkValidity()) {
-      //     this.#divisions = figura.value
-      //     this.dispatchEvent(new CustomEvent('change', { detail: { timeSignature: this.timeSignature } }))
-      //   }
-      // },
-
       change: (event) => {
         this.#divisions = event.detail.divisions
         this.dispatchEvent(new CustomEvent('change', { detail: { timeSignature: this.timeSignature } }))
@@ -156,17 +148,15 @@ export class TimeSignature extends HTMLElement {
     const shadow = this.shadowRoot
     const ul = shadow.querySelector('div.content ul')
     const tactus = shadow.querySelector('input#tactus')
-    // const figura = shadow.querySelector('input#figura')
-    const wheel = shadow.querySelector('yam-figura')
+    const figura = shadow.querySelector('yam-figura')
     const button = shadow.querySelector('[popovertarget]')
     const overlay = shadow.querySelector('div.overlay')
     const lock = shadow.querySelector('#lock')
 
     ul.addEventListener('click', this.#handlers.ul.click)
     tactus.addEventListener('input', this.#handlers.tactus.input)
-    // figura.addEventListener('input', this.#handlers.figura.input)
-    wheel.addEventListener('change', this.#handlers.figura.change)
-    wheel.addEventListener('changed', this.#handlers.figura.changed)
+    figura.addEventListener('change', this.#handlers.figura.change)
+    figura.addEventListener('changed', this.#handlers.figura.changed)
 
     overlay.addEventListener('click', this.#handlers.overlay.click)
     lock.addEventListener('animationend', this.#handlers.lock.animated)
@@ -199,24 +189,20 @@ export class TimeSignature extends HTMLElement {
     const shadow = this.shadowRoot
     const container = shadow.querySelector('div.time-signature')
     const tactus = shadow.querySelector('input#tactus')
-    // const figura = shadow.querySelector('input#figura')
-    const wheel = shadow.querySelector('yam-figura')
+    const figura = shadow.querySelector('yam-figura')
 
     if (v === '') {
       this.#timeSignature = ``
       tactus.value = ''
-      // figura.value = ''
-      wheel.divisions = 4
+      figura.divisions = 4
     } else if (v == 'common') {
       this.#timeSignature = `common`
       tactus.value = 4
-      // figura.value = 4
-      wheel.divisions = 4
+      figura.divisions = 4
     } else if (v == 'cut') {
       this.#timeSignature = `cut`
       tactus.value = 2
-      // figura.value = 2
-      wheel.divisions = 2
+      figura.divisions = 2
     } else {
       const { beats, divisions } = parse(`${v}`)
 
@@ -224,8 +210,7 @@ export class TimeSignature extends HTMLElement {
         this.#timeSignature = `${beats}:${divisions}`
 
         tactus.value = beats
-        // figura.value = divisions
-        wheel.divisions = divisions
+        figura.divisions = divisions
       }
     }
 
